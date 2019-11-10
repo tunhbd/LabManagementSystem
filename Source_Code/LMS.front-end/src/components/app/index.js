60ac3821e2d36c7f8dec89720d93da197fa1ccc1
Nguyen Huu Tu <tunhbd1998@gmail.com>
2019-11-10 09:30:24 +0700

    LMS(front-end): finish codebase

--- Source_Code/LMS.front-end/src/components/app/index.js
+++ Source_Code/LMS.front-end/src/components/app/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from '/media/images/logo/logo.svg'
+import './app.styles.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src="/media/images/logo/logo.svg"
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
